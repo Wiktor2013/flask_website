08dae1845e4d237786a2cc6b5400552546f67ffc
sanczo <wiktor_m@yahoo.com>
2021-09-14 23:44:37 +0200

    Initial commit

---
+++ website/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        dane = dict(request.form)
+        print(dane)
+        #return f"Thanks, {request.form.get('name')}"
+        with open("complaints.txt", "a") as f:
+            f.write(dane["name"] + ";" + dane["email"] + ";" + dane["complain"] + "\n")
+        return render_template("home.html", message=f"Thanks, {dane['name']}")
+
+    return render_template("home.html", message="")
